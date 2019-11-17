25157c68d16bab6eaa7227a62b776330498fe479
RV <razvanvancea94@gmail.com>
2019-11-17 17:14:22 +0200

    added searchbox

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ""
     };
   }
+
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(result => result.json())
       .then(users => this.setState({ monsters: users }));
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}></CardList>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
   }
