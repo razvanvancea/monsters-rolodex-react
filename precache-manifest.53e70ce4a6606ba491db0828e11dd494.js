526a70560690e1148ecb6da6e47eccdd0fafbbdb
RV <razvanvancea94@gmail.com>
2019-11-17 18:27:41 +0200

    Updates

---
+++ precache-manifest.53e70ce4a6606ba491db0828e11dd494.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4899fa829adb312bcc998439ccd7ef82",
+    "url": "/monsters-rolodex-react/index.html"
+  },
+  {
+    "revision": "ced6aa57006e7aee88b6",
+    "url": "/monsters-rolodex-react/static/css/main.623f6944.chunk.css"
+  },
+  {
+    "revision": "07904b6e04502fd4cb02",
+    "url": "/monsters-rolodex-react/static/js/2.e7a1b555.chunk.js"
+  },
+  {
+    "revision": "ced6aa57006e7aee88b6",
+    "url": "/monsters-rolodex-react/static/js/main.9d80196a.chunk.js"
+  },
+  {
+    "revision": "96fbe9edf3a55febe7f7",
+    "url": "/monsters-rolodex-react/static/js/runtime-main.7cf94afc.js"
+  }
+]);
